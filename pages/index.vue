fd4375f28a07b5fdffa617880aaf305d88119ec1
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-10-19 01:50:32 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -23,6 +23,37 @@
             </div>
         </div>    
         <div class="flex flex-col gap-20">
+            <div class="flex items-center gap-5">
+                <div class="bg-gradient-to-br from-[#48BBDE] to-[#B190F1] rounded-[25px] p-[3px] w-[35%]">
+                    <div class="flex flex-col items-center text-center py-[68px] w-full rounded-[25px] bg-white text-[#131313]">
+                        <p class="text-[96px]">150+</p>
+                        <p class="text-4xl leading-[135.3%] font-light -mt-2.5">вариантов: <br> квартиры, отели, дома</p>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-5 items-center w-[35%] text-white">
+                    <div class="flex flex-col rounded-[25px] py-[30px] px-20 bg-gradient-to-r from-[#d2b4f7] to-[#88d3ea] backdrop-blur-[50px]">
+                        <p class="text-4xl font-light">Работаем в</p>
+                        <div class="flex items-center gap-[30px]">
+                            <p class="text-[96px]">95</p>
+                            <p class="text-4xl leading-[135.3%] font-light">субъектах России</p>
+                        </div>
+                    </div>
+                    <div class="flex items-center rounded-[23px] justify-center py-[33.5px] w-full bg-gradient-to-r from-[#d2b4f7] to-[#88d3ea]">
+                        <p class="text-[29px] leading-[135.3%] font-light">Стоимость без наценки</p>
+                    </div>
+                </div>
+                <div class="flex flex-col items-center gap-5 w-[27%] text-white">
+                    <div class="flex items-center justify-center w-full py-[38px] bg-gradient-to-r from-[#d2b4f7] to-[#88d3ea] rounded-[25px]">
+                        <p class="text-[29px] leading-[135.3%] font-light text-center">Удобная система бронирования жилья</p>
+                    </div>
+                    <div class="bg-gradient-to-br from-[#48BBDE] to-[#B190F1] rounded-[25px] p-[3px] w-full">
+                        <div class="flex flex-col items-center text-center py-[21px] w-full rounded-[25px] bg-white text-[#131313]">
+                            <p class="text-[64px]">24/7</p>
+                            <p class="text-2xl leading-[135.3%] font-light -mt-2.5">Служба поддержки <br> на связи!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="flex flex-col gap-3 rounded-[35px] text-white why_gradient py-[50px]">
                 <div class="w-4/5 mx-auto flex items-start justify-between">
                     <div class="flex flex-col gap-4">
