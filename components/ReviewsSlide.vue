a41df4186e334e39f79bd2f95bb65e72a48f0453
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-11-04 13:10:55 +0300

    upd

--- components/ReviewsSlide.vue
+++ components/ReviewsSlide.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-gradient-to-br from-[#48BBDE] to-[#B190F1] rounded-[35px] p-[3px] flex items-center justify-center dark:from-[#3E3E3E] dark:to-[#3E3E3E] h-full">        
+    <div class="bg-gradient-to-l from-[#48BBDE] to-[#B190F1] rounded-[35px] p-[3px] flex items-center justify-center dark:from-[#3E3E3E] dark:to-[#3E3E3E] h-full">        
         <div class="flex flex-col gap-4 md:gap-6 xl:gap-[30px] rounded-[35px] p-5 md:p-7 xl:p-10 text-[#131313] dark:text-white h-full justify-between bg-white dark:bg-[#3E3E3E]">
             <div class="flex items-center gap-2.5 flex-wrap w-full">
                 <p class="rounded-full leading-[135.3%] py-1 px-[30px] dark:bg-white/30 border-2 border-[#131313] dark:border-white" v-for="advantage in props.advantages">{{ advantage }}</p>
