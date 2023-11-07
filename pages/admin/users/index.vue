79aaad8094edcd9e675297166d0326aa46521120
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-11-07 04:03:17 +0300

    upd

--- pages/admin/users/index.vue
+++ pages/admin/users/index.vue
@@ -1,6 +1,38 @@
 <template>
     <div class="flex flex-col gap-5 py-10">
         <p class="text-lg md:text-xl xl:text-2xl leading-[135.3%]">Пользователи</p>
+        <div class="grid grid-cols-4 gap-x-24 gap-y-5 rounded-[25px] bg-[#D9D9D9] px-12 py-7">
+            <div class="col-span-full grid grid-cols-4 gap-x-24 pb-4 border-b border-black">
+                <p>Имя</p>
+                <p>Почта</p>
+                <p>Телефон</p>
+                <p>Дата регистрации</p>
+            </div>
+            <p>Иванов Иван Иванович</p>
+            <p>Ivanov@gmail.com</p>
+            <p>+79992352345</p>
+            <p>24.02.2012, 17.47</p>
+            <p>Иванов Иван Иванович</p>
+            <p>Ivanov@gmail.com</p>
+            <p>+79992352345</p>
+            <p>24.02.2012, 17.47</p>
+            <p>Иванов Иван Иванович</p>
+            <p>Ivanov@gmail.com</p>
+            <p>+79992352345</p>
+            <p>24.02.2012, 17.47</p>
+            <p>Иванов Иван Иванович</p>
+            <p>Ivanov@gmail.com</p>
+            <p>+79992352345</p>
+            <p>24.02.2012, 17.47</p>
+            <p>Иванов Иван Иванович</p>
+            <p>Ivanov@gmail.com</p>
+            <p>+79992352345</p>
+            <p>24.02.2012, 17.47</p>
+            <p>Иванов Иван Иванович</p>
+            <p>Ivanov@gmail.com</p>
+            <p>+79992352345</p>
+            <p>24.02.2012, 17.47</p>
+        </div>
         <div class="relative overflow-x-auto">
             <!-- <table class="bg-[#D9D9D9] rounded-[25px] py-8 w-full flex flex-col">
                 <thead class="flex px-12 w-full">
