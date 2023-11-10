be15341b6fe11912f7a5fadf3352b05c22c86b66
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-11-10 04:19:40 +0300

    upd

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col gap-5 pt-10">
         <div class="flex md:items-end md:justify-between max-md:flex-col">
             <p class="text-lg md:text-xl xl:text-2xl leading-[135.3%]">Главное меню</p>
-            <NuxtLink to="/" class="flex items-center w-fit py-2 px-5 gap-2.5 text-base md:text-lg xl:text-xl leading-[135.3%] border border-black rounded-full bg-[#D9D9D9]">
+            <NuxtLink to="/admin/add" class="flex items-center w-fit py-2 px-5 gap-2.5 text-base md:text-lg xl:text-xl leading-[135.3%] border border-black rounded-full bg-[#D9D9D9]">
                 <span>Добавить недвижимость</span>                
                 <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
                     <path d="M10.5 1V20" stroke="black" stroke-linecap="round"/>
@@ -16,6 +16,12 @@
                     </thead>
                 </table>
             </div> -->
+            {{ data }}
         </div>
     </div>
 </template>
+
+<script setup>
+    const config = useRuntimeConfig()
+    const { data, error } = await useFetch(`${config.public.APIbaseURL}/getApartments`)
+</script>
