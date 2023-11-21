d229a41bd33648461b88da9c846854585417f0bf
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-11-21 15:47:19 +0300

    upd

--- pages/search.vue
+++ pages/search.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="my-10 flex flex-col gap-5 w-full">
-        <pre>{{ apartments }}</pre>
-        <div class="w-full py-6 xl:py-8 px-6 md:px-12 xl:px-[90px] bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] rounded-[25px] flex items-center gap-8 md:gap-12 xl:gap-48">
+        <!-- <pre>{{ apartments }}</pre> -->
+        <!-- <div class="w-full py-6 xl:py-8 px-6 md:px-12 xl:px-[90px] bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] rounded-[25px] flex items-center gap-8 md:gap-12 xl:gap-48">
             <p class="NeutralFace text-2xl md:text-3xl xl:text-[32px] leading-[151.8%] uppercase text-white">отели в москве</p>
             <div class="flex items-center gap-5">
                 <div class="relative">
@@ -28,74 +28,88 @@
                 </div>
                 <input type="text" class="rounded-[10px] bg-[#EBEBEB] border border-[#B1B1B1] py-2.5 px-5 text-[#696969] w-[183px]" placeholder="Гости, номера">
             </div>
+        </div> -->
+        <div class="w-full py-6 rounded-[30px] border border-[#7B7B7B] dark:border-white bg-white">
+            <div class="px-6 md:px-12 xl:px-[90px] grid grid-cols-1 lg:grid-cols-4 gap-5 font-light">
+                <input v-model="city" type="text" class="px-6 py-4 rounded-[15px] border border-[#B1B1B1] bg-[#EBEBEB] dark:bg-transparent placeholder-[#B1B1B1]" placeholder="Куда едем?">
+                <div class="flex items-center bg-[#EBEBEB] dark:bg-transparent rounded-[15px] border border-[#B1B1B1] relative">
+                    <input :value="dateFrom" @input="dateFrom = $event.target.value" type="date" class="w-1/2 py-4 px-6 bg-[#EBEBEB] dark:bg-transparent rounded-l-[15px] focus:outline-none focus:ring-0 cursor-pointer">
+                    <input :value="dateTo" @input="dateTo = $event.target.value" type="date" class="w-1/2 py-4 px-6 bg-[#EBEBEB] dark:bg-transparent rounded-r-[15px] focus:outline-none focus:ring-0 cursor-pointer">
+                    <div class="absolute w-px h-full top-0 left-1/2 bg-[#B1B1B1]"></div>
+                </div>
+                <input type="text" class="px-6 py-4 rounded-[15px] border border-[#B1B1B1] bg-[#EBEBEB] dark:bg-transparent placeholder-[#B1B1B1]" placeholder="Гости, номера">
+                <button class="py-[14px] text-center text-white rounded-[15px] bg-gradient-to-l from-[#B98CF2] to-[#40BDDB] text-2xl transition-all duration-500 hover:shadow-[4px_4px_8px_0px_rgba(0,0,0,0.25)_inset]">Найти</button>
+            </div>
         </div>
-        <div class="flex items-start gap-5 w-full">
-            <div class="w-[20%] bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] p-[2px] rounded-[25px]">
-                <div class="py-6 md:py-8 xl:py-10 px-4 xl:px-7 bg-white rounded-[25px] w-full h-full flex flex-col gap-5">
-                    <div class="flex flex-col gap-4">
-                        <p class="font-light leading-[135.3%] text-base">Цена (₽)</p>
-                        <div class="w-full relative flex rounded-[10px] border border-[#B1B1B1] overflow-hidden">
-                            <input v-model="filters.minPrice" type="text" class="py-3 px-4 w-1/2 bg-[#EBEBEB] rounded-l-[10px]" placeholder="от 2000">
-                            <input v-model="filters.maxPrice" type="text" class="py-3 px-4 w-1/2 bg-[#EBEBEB] rounded-r-[10px]" placeholder="от 1500000">
-                            <div class="h-full w-px top-0 left-1/2 -translate-x-1/2 bg-[#B1B1B1] absolute"></div>
-                        </div>
-                    </div>
-                    <div class="flex flex-col gap-2.5">
-                        <p>Метро</p>
-                        <div class="flex flex-col gap-0.5">
-                            <select class="focus:outline-none focus:right-0" v-model="filters.station">
-                                <option value="Все" selected>Все</option>
-                                <option v-for="station in stations" :value="station">{{ station }}</option>
-                            </select>
-                            <div class="w-full h-px rounded-full bg-gradient-to-r from-[#B98CF2] to-[#48BBDE]"></div>
-                        </div>
-                    </div>
-                    <button @click="filterApartments" class="text-white py-2 px-4 rounded-full bg-gradient-to-r from-[#B98CF2] to-[#48BBDE]">Применить</button>
-                </div>
-            </div>
-            <div class="w-[50%] flex flex-col gap-5">
-                <div class="flex items-center justify-between gap-4 leading-[135.3%] font-light">
-                    <p class="text-2xl">Выбранные фильтры:</p>
-                    <button @click="removeFilter" class="text-base relative after:absolute after:w-full after:h-px after:rounded-full after:bg-gradient-to-r after:from-[#B98CF2] after:to-[#48BBDE] after:left-0 after:bottom-0">Сбросить все фильтры</button>
-                </div>
-                <div class="flex items-center justify-between gap-4 leading-[135.3%] font-light">
-                    <p class="text-2xl">Найдено {{ apartments.length }} вариантов</p>
-                    <div class="flex items-center gap-7">
-                        <p>Сортировка</p>
-                        <select class="focus:outline-none focus:right-0">
-                            <option value="" disabled selected>По популярности</option>
-                            <option value="">Выбор 1</option>
-                            <option value="">Выбор 2</option>
-                            <option value="">Выбор 3</option>
-                            <option value="">Выбор 4</option>
-                            <option value="">Выбор 5</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] p-[1px] rounded-[20px]" v-for="apartment in apartments">
-                    <div class="p-3 bg-white rounded-[20px] w-full h-full flex items-start justify-between gap-5">
-                        <div class="flex flex-col gap-2" v-if="apartment.image">
-                            <img :src="`${config.public.APIbaseURL}/${image.path}`" alt="" v-for="image in apartment.image" class="w-full md:w-[300px] max-w-max object-cover aspect-video">
-                        </div>
-                        <div class="w-full h-72 rounded-xl bg-[#7C7C7C]" v-else></div>
-                        <div class="flex flex-col gap-2.5 leading-[135.3%]">
-                            <p class="text-xl font-normal">{{ apartment.city }}</p>
-                            <p class="font-light text-base">{{ apartment.address }}</p>
-                        </div>
-                        <div class="flex flex-col gap-1 items-end">
-                            <div class="bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] p-[1px] rounded-[5px]">
-                                <div class="relative py-2 pl-4 pr-20 font-light leading-[135.3%] rounded-[5px] bg-[#D9D9D9] w-full h-full">
-                                    <p class="text-sm">Превосходно</p>
-                                    <p class="text-base text-white py-1.5 px-4 absolute top-1/2 -translate-y-1/2 right-0 bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] rounded-[5px]">8.8</p>
-                                </div>
+        <div class="flex flex-col gap-5"> 
+            <div class="flex items-start gap-5 w-full">
+                <div class="w-[30%] bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] p-[2px] rounded-[25px]">
+                    <div class="py-6 md:py-8 xl:py-10 px-4 xl:px-7 bg-white rounded-[25px] w-full h-full flex flex-col gap-5">
+                        <div class="flex flex-col gap-4">
+                            <p class="font-light leading-[135.3%] text-base">Цена (₽)</p>
+                            <div class="w-full relative flex rounded-[10px] border border-[#B1B1B1] overflow-hidden">
+                                <input v-model="filters.minPrice" type="text" class="py-3 px-4 w-1/2 bg-[#EBEBEB] rounded-l-[10px]" placeholder="от 2000">
+                                <input v-model="filters.maxPrice" type="text" class="py-3 px-4 w-1/2 bg-[#EBEBEB] rounded-r-[10px]" placeholder="от 1500000">
+                                <div class="h-full w-px top-0 left-1/2 -translate-x-1/2 bg-[#B1B1B1] absolute"></div>
                             </div>
-                            <p class="text-[10px] text-[#696969]">256 отзывов</p>
-                            <p>{{ apartment.pricePerDay }}</p>
+                        </div>
+                        <div class="flex flex-col gap-2.5">
+                            <p>Метро</p>
+                            <div class="flex flex-col gap-0.5">
+                                <select class="focus:outline-none focus:right-0" v-model="filters.station">
+                                    <option value="Все" selected>Все</option>
+                                    <option v-for="station in stations" :value="station">{{ station }}</option>
+                                </select>
+                                <div class="w-full h-px rounded-full bg-gradient-to-r from-[#B98CF2] to-[#48BBDE]"></div>
+                            </div>
+                        </div>
+                        <button @click="filterApartments" class="text-white py-2 px-4 rounded-full bg-gradient-to-r from-[#B98CF2] to-[#48BBDE]">Применить</button>
+                    </div>
+                </div>
+                <div class="w-[70%] flex flex-col gap-5">
+                    <div class="flex items-center justify-between gap-4 leading-[135.3%] font-light">
+                        <p class="text-2xl">Выбранные фильтры:</p>
+                        <button @click="removeFilter" class="text-base relative after:absolute after:w-full after:h-px after:rounded-full after:bg-gradient-to-r after:from-[#B98CF2] after:to-[#48BBDE] after:left-0 after:bottom-0">Сбросить все фильтры</button>
+                    </div>
+                    <div class="flex items-center justify-between gap-4 leading-[135.3%] font-light">
+                        <p class="text-2xl">Найдено {{ apartments.length }} вариантов</p>
+                        <div class="flex items-center gap-7">
+                            <p>Сортировка</p>
+                            <select class="focus:outline-none focus:right-0">
+                                <option value="" disabled selected>По популярности</option>
+                                <option value="">Выбор 1</option>
+                                <option value="">Выбор 2</option>
+                                <option value="">Выбор 3</option>
+                                <option value="">Выбор 4</option>
+                                <option value="">Выбор 5</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] p-[1px] rounded-[20px]" v-for="apartment in apartments">
+                        <div class="p-3 bg-white rounded-[20px] w-full h-full flex items-start justify-between gap-5">
+                            <div class="flex flex-col gap-2" v-if="apartment.image">
+                                <img :src="`${config.public.APIbaseURL}/${image.path}`" alt="" v-for="image in apartment.image" class="w-full md:w-[300px] max-w-max object-cover aspect-video rounded-[13px]">
+                            </div>
+                            <div class="w-full h-72 rounded-xl bg-[#7C7C7C]" v-else></div>
+                            <div class="flex flex-col gap-2.5 leading-[135.3%] grow">
+                                <p class="text-xl font-normal">{{ apartment.city }}</p>
+                                <p class="font-light text-base">{{ apartment.address }}</p>
+                            </div>
+                            <div class="flex flex-col gap-1 items-end">
+                                <div class="bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] p-[1px] rounded-[5px]">
+                                    <div class="relative py-2 pl-4 pr-20 font-light leading-[135.3%] rounded-[5px] bg-[#D9D9D9] w-full h-full">
+                                        <p class="text-sm">Превосходно</p>
+                                        <p class="text-base text-white py-1.5 px-4 absolute top-1/2 -translate-y-1/2 right-0 bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] rounded-[5px]">8.8</p>
+                                    </div>
+                                </div>
+                                <p class="text-[10px] text-[#696969]">256 отзывов</p>
+                                <p>{{ apartment.pricePerDay }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="w-[30%] bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] p-[1px] rounded-[25px]">
+            <div class="w-full bg-gradient-to-r from-[#B98CF2] to-[#48BBDE] p-[1px] rounded-[25px]">
                 <div class="w-full h-full rounded-[25px] bg-white overflow-hidden">
                     <ClientOnly>
                         <YandexMap :coordinates="[55.755864, 37.617698]" :zoom="10">
@@ -123,7 +137,7 @@
     const { data, error } = await useFetch(`${config.public.APIbaseURL}/api/admin/getApartments`)
 
     /* create filters */
-    const apartments =ref(data.value)
+    const apartments = ref(data.value)
     const { city, dateFrom, dateTo} = storeToRefs(useSearchStore())
     
     /* add undergrou, price, create filters */
@@ -163,7 +177,7 @@
 
 <style>
     .yandex-container {
-        height: 700px;
+        height: 400px;
     }
     .yandex-balloon {
         height: 60px;
